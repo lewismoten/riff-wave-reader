8f13564b06aacfafa76b3f0b5f8afca9b6d161c4
Lewis Moten <lewismoten@gmail.com>
2019-05-04 11:46:43 -0400

    refactor: start at calculated position (#1)

--- read-sample.js
+++ read-sample.js
@@ -5,18 +5,12 @@ import {
   errorOpeningFile,
   errorSampleNotFound,
   errorSampleTruncated
-  // errorDataId
 } from "./en-us.js";
 
 const read = util.promisify(fs.read);
 
 /*
-
 const getChannelSample = (buffer, channel, index) => {
-  const bitsPerSample = getBitsPerSample(buffer);
-  const channels = getChannels(buffer);
-  const chunkSize = (channels * bitsPerSample) / 8;
-  const offset = 44 + index * chunkSize + channel * bitsPerSample;
   if (bitsPerSample === 8) return buffer.readUInt8(offset);
   if (bitsPerSample === 16) return buffer.readInt16LE(offset);
   if (bitsPerSample === 32) return buffer.readInt32LE(offset);
@@ -30,51 +24,22 @@ const getSampleValueRange = bitsPerSample => {
 
 export const readSample = (context, channel, index) =>
   new Promise((resolve, reject) => {
-    context.readFormat().then(({ sampleSize, bitsPerSample }) => {
+    context.readFormat().then(({ sampleStart, sampleSize, bitsPerSample }) => {
       fs.open(context.file, "r", (openError, fileDescriptor) => {
         if (openError) {
           reject(errorOpeningFile);
         } else {
-          // fix position start to calculate later. currently 44 is correct for this file
           const position =
-            44 + index * sampleSize + (channel * bitsPerSample) / 8;
-          console.log(
-            "Reading channel %s index %s at position %s",
-            channel,
-            index,
-            position
-          );
+            sampleStart + index * sampleSize + (channel * bitsPerSample) / 8;
           const size = bitsPerSample / 8;
           const myBuffer = Buffer.alloc(size);
           const bufferPosition = 0;
           const validateBytesRead = ({ buffer, bytesRead }) => {
-            // console.log(
-            //   "ARGSSSSSSSSSSSSSSSSS",
-            //   JSON.stringify(
-            //     {
-            //       buffer,
-            //       bytesRead,
-            //       size,
-            //       position,
-            //       index,
-            //       sampleSize,
-            //       channel,
-            //       bitsPerSample,
-            //       FOO: 44 + index * sampleSize
-            //     },
-            //     null,
-            //     "  "
-            //   )
-            // );
             if (bytesRead === 0) throw errorSampleNotFound;
             if (bytesRead < buffer.length) throw errorSampleTruncated;
             return buffer;
           };
           const readSample = buffer => {
-            // console.log(
-            //   "ARGSSSSSSSSSSSSSSSSS",
-            //   JSON.stringify(buffer, null, "  ")
-            // );
             return buffer.readUInt8(0);
           };
           read(fileDescriptor, myBuffer, bufferPosition, size, position)
