a15a45306aaa3801712a1134f3478153f0645e66
Lewis Moten <lewismoten@gmail.com>
2019-05-04 20:14:21 -0400

    fix: reject exceptions (#13)

--- src/read-riff--header.js
+++ src/read-riff--header.js
@@ -5,20 +5,23 @@ export const readRiffHeader = context =>
     if (context.riff) {
       resolve(context.riff);
     }
-    context.getBuffer(0, 12).then(buffer => {
-      const tag = buffer.toString("ascii", 0, 4);
-      const size = buffer.readInt32LE(4);
-      const format = buffer.toString("ascii", 8, 12);
-      if (tag !== "RIFF") reject(errorRiffTag);
-      else if (size < 40) reject(errorRiffSize);
-      else if (format !== "WAVE") reject(errorRiffFormat);
-      else
-        resolve(
-          (context.riff = {
-            tag,
-            size,
-            format
-          })
-        );
-    });
+    return context
+      .getBuffer(0, 12)
+      .then(buffer => {
+        const tag = buffer.toString("ascii", 0, 4);
+        const size = buffer.readInt32LE(4);
+        const format = buffer.toString("ascii", 8, 12);
+        if (tag !== "RIFF") reject(errorRiffTag);
+        else if (size < 40) reject(errorRiffSize);
+        else if (format !== "WAVE") reject(errorRiffFormat);
+        else
+          resolve(
+            (context.riff = {
+              tag,
+              size,
+              format
+            })
+          );
+      })
+      .catch(reject);
   });
