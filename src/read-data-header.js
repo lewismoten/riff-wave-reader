ebf97c029a7fc9e9de6a5cc89e0af1cfd1ecceb0
Lewis Moten <lewismoten@gmail.com>
2019-05-04 21:38:44 -0400

    refactor: data header from buffer (#13)

--- src/read-data-header.js
+++ src/read-data-header.js
@@ -1,13 +1,4 @@
-const fs = require("fs");
-const util = require("util");
-
-import {
-  errorOpeningFile,
-  errorDataHeaderTruncated,
-  errorDataId
-} from "./en-us.js";
-
-const read = util.promisify(fs.read);
+import { errorDataId } from "./en-us.js";
 
 export const readDataHeader = context =>
   new Promise((resolve, reject) => {
@@ -15,47 +6,16 @@ export const readDataHeader = context =>
       resolve(context.dataHeader);
     } else {
       context.readFormat().then(({ size: formatSize }) => {
-        fs.open(context.file, "r", (openError, fileDescriptor) => {
-          if (openError) {
-            reject(errorOpeningFile);
-          } else {
-            const position = 20 + formatSize;
-            const size = 8;
-            const myBuffer = Buffer.alloc(size);
-            const bufferPosition = 0;
-
-            const validateBytesRead = ({ buffer, bytesRead }) => {
-              if (bytesRead < buffer.length) throw errorDataHeaderTruncated;
-              return { buffer, target: {} };
-            };
-            const readId = ({ buffer, target }) => {
-              const id = buffer.toString("ascii", 0, 4);
-              if (id !== "data") throw errorDataId;
-              target.id = id;
-              return { buffer, target };
-            };
-            const readSize = ({ buffer, target }) => {
-              const size = buffer.readInt32LE(4);
-              target.size = size;
-              return { buffer, target };
-            };
-
-            const cacheResults = ({ target }) => (context.dataHeader = target);
-            const calculateStart = ({ buffer, target }) => {
-              target.start = position + size;
-              return { buffer, target };
-            };
-
-            read(fileDescriptor, myBuffer, bufferPosition, size, position)
-              .then(validateBytesRead)
-              .then(readId)
-              .then(readSize)
-              .then(calculateStart)
-              .then(cacheResults)
-              .then(resolve)
-              .catch(reject);
-          }
-        });
+        context
+          .getBuffer(20 + formatSize, 8)
+          .then(buffer => {
+            const id = buffer.toString("ascii", 0, 4);
+            const size = buffer.readInt32LE(4);
+            const start = 20 + formatSize + 8;
+            if (id !== "data") reject(errorDataId);
+            else resolve((context.dataHeader = { id, size, start }));
+          })
+          .catch(reject);
       });
     }
   });
