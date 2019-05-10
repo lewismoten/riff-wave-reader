fe043e1f1cf551d170e3aeb329c001da5281d45f
Lewie <lewismoten@gmail.com>
2019-05-10 19:41:11 -0400

    test: test 16 bit headers
    issue #2

--- spec/sample-chunks.spec.js
+++ spec/sample-chunks.spec.js
@@ -18,6 +18,7 @@ describe("Sample Headers", () => {
   it(`8-bit-16khz-mono-04.wav`, () => hasExpectedHeaders("8-bit-16khz-mono-04"));
   it(`8-bit-16khz-stereo-01.wav`, () => hasExpectedHeaders("8-bit-16khz-stereo-01"));
   it(`8-bit-16khz-stereo-02.wav`, () => hasExpectedHeaders("8-bit-16khz-stereo-02"));
+  it(`16-bit-4khz-mono-01.wav`, () => hasExpectedHeaders("16-bit-4khz-mono-01"));
 });
 const hasExpectedHeaders = name => {
   return new RiffWaveReader(new Reader(path.join(__dirname, `../samples/${name}.wav`)))
