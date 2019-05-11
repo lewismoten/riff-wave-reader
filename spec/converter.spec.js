cc7936ea17be2d5abf2b7c8349bdb3a4d61a3a13
Lewie <lewismoten@gmail.com>
2019-05-11 15:55:20 -0400

    test: uint16
    issue #2

--- spec/converter.spec.js
+++ spec/converter.spec.js
@@ -1,5 +1,6 @@
 import {
-  uint8
+  uint8,
+  uint16
 } from "../src/converter.js";
 
 describe("converter", () => {
@@ -12,6 +13,17 @@ describe("converter", () => {
     });
     it("starts from specified position", () => {
       expect(uint8([0x00, 0x01, 0x02], 1)).toBe(1);
+    });
+  });
+  describe("uint16", () => {
+    it("converts min value", () => {
+      expect(uint16([0x00, 0x00], 0)).toBe(0);
+    });
+    it("converts max value", () => {
+      expect(uint16([0xFF, 0xFF], 0)).toBe(65535);
+    });
+    it("starts from specified position", () => {
+      expect(uint16([0x00, 0x01, 0x02, 0x03], 1)).toBe(513);
     });
   });
 })
