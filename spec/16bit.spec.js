0be7b87e033e2406759e520794ba623c6055295b
Lewie <lewismoten@gmail.com>
2019-05-10 21:09:00 -0400

    test: 16 bit sample values
    issue #2

---
+++ spec/16bit.spec.js
@@ -0,0 +1,29 @@
+import RiffWaveReader from "../src/index.js";
+import Reader from "../src/reader.js";
+
+import path from "path";
+
+const file = path.join(__dirname, "../samples/16-bit-4khz-mono-01.wav");
+const mono = 0;
+
+describe("16 bit wave file", () => {
+      describe("read last samples", () => {
+        let reader;
+        let sampleCount;
+        const lastSamples = [
+          0x81, 0x81, 0x81, 0x80, 0x80, 0x80, 0x7f, 0x7f,
+          0x7e, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e, 0x7e
+        ];
+        beforeAll((done) => {
+          reader = new RiffWaveReader(new Reader(file));
+          return reader.readChunks().then(({ data }) => { sampleCount = data.sampleCount; done()})
+        });
+        for (let i = 0; i < lastSamples.length; i++) {
+          it(`can read last sample ${i + 1} of ${lastSamples.length}`, () => reader
+                  .readSample(mono, sampleCount - lastSamples.length + i)
+                  .then(actualValue => {
+                    expect(actualValue).toBe(lastSamples[i]);
+                  }));
+        }
+      })
+  });
