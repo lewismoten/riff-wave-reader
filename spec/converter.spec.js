b4ceb55bbf0399aac5b05d305c196a6001597bb5
Lewie <lewismoten@gmail.com>
2019-05-11 16:02:33 -0400

    test: int16
    issue #2

--- spec/converter.spec.js
+++ spec/converter.spec.js
@@ -1,6 +1,7 @@
 import {
   uint8,
-  uint16
+  uint16,
+  int16
 } from "../src/converter.js";
 
 describe("converter", () => {
@@ -24,6 +25,23 @@ describe("converter", () => {
     });
     it("starts from specified position", () => {
       expect(uint16([0x00, 0x01, 0x02, 0x03], 1)).toBe(513);
+    });
+  });
+  describe("int16", () => {
+    it("converts from min hex value", () => {
+      expect(int16([0x00, 0x00], 0)).toBe(0);
+    });
+    it("converts from max hex value", () => {
+      expect(int16([0xFF, 0xFF], 0)).toBe(-1);
+    });
+    it("converts to min int16 value", () => {
+      expect(int16([0x00, 0x80], 0)).toBe(-32768);
+    });
+    it("converts to max int16 value", () => {
+      expect(int16([0xFF, 0x7F], 0)).toBe(32767);
+    });
+    it("starts from specified position", () => {
+      expect(int16([0x00, 0x01, 0x02, 0x03], 1)).toBe(513);
     });
   });
 })
