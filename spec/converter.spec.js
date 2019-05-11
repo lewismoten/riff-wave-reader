c35dbe129d34793480ce27420e3242d55d774cc3
Lewie <lewismoten@gmail.com>
2019-05-11 17:48:01 -0400

    test: show bits in tests
    issue #2

--- spec/converter.spec.js
+++ spec/converter.spec.js
@@ -28,20 +28,27 @@ describe("converter", () => {
     });
   });
   describe("int16", () => {
-    it("converts from min hex value", () => {
-      expect(int16([0x00, 0x00], 0)).toBe(0);
+    // second byte, first bit indicates signed state
+    it("converts to min value", () => {
+      expect(int16([0b00000000, 0b10000000], 0)).toBe(-32768);
     });
-    it("converts from max hex value", () => {
-      expect(int16([0xFF, 0xFF], 0)).toBe(-1);
+    it("converts to -2", () => {
+      expect(int16([0b11111110, 0b11111111], 0)).toBe(-2);
     });
-    it("converts to min int16 value", () => {
-      expect(int16([0x00, 0x80], 0)).toBe(-32768);
+    it("converts from max value", () => {
+      expect(int16([0b11111111, 0b11111111], 0)).toBe(-1);
     });
-    it("converts to max int16 value", () => {
-      expect(int16([0xFF, 0x7F], 0)).toBe(32767);
+    it("converts from min value", () => {
+      expect(int16([0b00000000, 0b00000000], 0)).toBe(0);
+    });
+    it("converts to 1", () => {
+      expect(int16([0b00000001, 0b00000000], 0)).toBe(1);
     });
     it("starts from specified position", () => {
       expect(int16([0x00, 0x01, 0x02, 0x03], 1)).toBe(513);
+    });
+    it("converts to max value", () => {
+      expect(int16([0b11111111, 0b01111111], 0)).toBe(32767);
     });
   });
 })
